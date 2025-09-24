522091c8e9e2b561f74decf55835f8bf72222d8a
INTERNAL\Per.Karlsson <per.karlsson@visma.com>
2025-09-24 16:52:01 +0200

    Update Next.js configuration to enable static export, set trailing slashes, and specify output directory. Adjust build script in package.json to remove unnecessary export command for static builds.

--- rpac-web/next.config.js
+++ rpac-web/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { unoptimized: true },
+  output: 'export',
+  trailingSlash: true,
+  distDir: 'out',
 };
 
 module.exports = nextConfig;
